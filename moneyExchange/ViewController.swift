f970281d1026819bdf7f544f8a7cd7e7878e6515
Alex <alex@Alexs-iMac.local>
2018-10-29 22:33:58 +0100

    first commit

---
+++ moneyExchange/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  moneyExchange
+//
+//  Created by Alex on 29/10/2018.
+//  Copyright © 2018 Alex. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
